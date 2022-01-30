3a60decb34b4451a1864863192afbf707686cc58
Ben James <39443778+Benjames01@users.noreply.github.com>
2022-01-30 17:20:06 +0000

    Revert "Uptt 4 - Random Height Generation"

--- Assets/UPTT/Editor/UPTerrainEditor.cs
+++ Assets/UPTT/Editor/UPTerrainEditor.cs
@@ -8,55 +8,14 @@ using UnityEngine;
 [CanEditMultipleObjects]
 public class UPTerrainEditor : Editor
 {
-    // UPTerrain properties
-    private SerializedProperty _randomHeightRange;
-    
-    // Editor Foldouts
-    private bool _showRandom = true;
-    
-    
     private void OnEnable()
     {
-        // Get the property from the UPTerrain script
-        _randomHeightRange = serializedObject.FindProperty("randomHeightRange");
-        
         
     }
 
     public override void OnInspectorGUI()
     {
-        // Update all of the serialised values between the editor and the terrain object
-        serializedObject.Update();
-
-        UPTerrain terrain = (UPTerrain) target;
         
-        
-        // Show ResetButton
-        GUILayout.Label("Reset Terrain", EditorStyles.boldLabel);
-
-        if (GUILayout.Button("Reset Terrain"))
-        {
-            terrain.ResetTerrain();
-        }
-        
-
-        // Random Height Foldout UI
-        _showRandom = EditorGUILayout.Foldout(_showRandom, "Random Height");
-        if (_showRandom)
-        {
-            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
-            GUILayout.Label("Set heights between given range.", EditorStyles.boldLabel);
-            EditorGUILayout.PropertyField(_randomHeightRange);
-            
-            if(GUILayout.Button("Generate Random Heights"))
-            {
-                terrain.RandomTerrain();
-            }
-            
-        }
-        
-        // Apply the changes made to the object
-        serializedObject.ApplyModifiedProperties();
     }
 
     private void Start()
