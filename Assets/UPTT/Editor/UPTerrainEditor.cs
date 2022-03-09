b2d1ca7c4ab75ec54e0702496f376b0c8d4953be
Ben James <39443778+Benjames01@users.noreply.github.com>
2022-03-09 16:21:44 +0000

    Add perlin superposition using fBM (#11)

--- Assets/UPTT/Editor/UPTerrainEditor.cs
+++ Assets/UPTT/Editor/UPTerrainEditor.cs
@@ -1,4 +1,5 @@
 using System;
+using EditorGUITable;
 using UnityEditor;
 using UnityEditor.Experimental.TerrainAPI;
 using UnityEngine;
@@ -14,10 +15,29 @@ public class UPTerrainEditor : Editor
     // Height Map settings
     private SerializedProperty _heightMapTexture;
     private SerializedProperty _heightMapScale;
+    private SerializedProperty _replaceHeightMap;
+    
+    // Perlin/ fBM settings
+    private SerializedProperty _seed;
+    private SerializedProperty _perlinScaleFactor;
+    private SerializedProperty _perlinHeightScale;
+    private SerializedProperty _octaves;
+    private SerializedProperty _persistence;
+
+    private GUITableState _perlinSettingsTable;
+    private SerializedProperty _perlinSettingsList;
+    
     
     // Editor Foldouts
-    private bool _showRandom, _showHeightMapProperties = true;
+    private bool _showRandom, _showHeightMapProperties, _showPerlin, _showfBM, _showVoronoi = false;
     
+    
+    
+    /**
+     *  [SerializeField] private int seed;
+     *  [SerializeField] private Vector2 perlinScaleFactor;
+     *  [SerializeField] private bool replaceHeightmap = false;
+     */
     
     private void OnEnable()
     {
@@ -25,8 +45,17 @@ public class UPTerrainEditor : Editor
         _unityRandomRange = serializedObject.FindProperty("unityRandomRange");
         _heightMapTexture = serializedObject.FindProperty("heightMapTexture");
         _heightMapScale   = serializedObject.FindProperty("heightMapScale");
+        _replaceHeightMap   = serializedObject.FindProperty("replaceHeightmap");
         
-        
+        _seed               = serializedObject.FindProperty("seed");
+        _perlinScaleFactor  = serializedObject.FindProperty("perlinScaleFactor");
+        _perlinHeightScale  = serializedObject.FindProperty("perlinHeightScale");
+        _octaves            = serializedObject.FindProperty("octaves");
+        _persistence        = serializedObject.FindProperty("persistence");
+
+        _perlinSettingsTable = new GUITableState("perlinSettingsTable");
+        _perlinSettingsList = serializedObject.FindProperty("perlinNoiseSettingsList");
+
     }
 
     public override void OnInspectorGUI()
@@ -63,14 +92,67 @@ public class UPTerrainEditor : Editor
         if (_showRandom)
         {
             EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
-            GUILayout.Label("Set heights between given range.", EditorStyles.boldLabel);
+            GUILayout.Label("Set heights between given range", EditorStyles.boldLabel);
             EditorGUILayout.PropertyField(_unityRandomRange);
-            
+            EditorGUILayout.PropertyField(_replaceHeightMap);
             if(GUILayout.Button("Generate Random Heights"))
             {
                 terrain.UnityRandomHeightMap();
             }
+        }
+        
+        // Perlin Foldout UI
+        _showPerlin = EditorGUILayout.Foldout(_showPerlin, "Perlin Noise HeightMap Generator");
+        if (_showPerlin)
+        {
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
             
+            GUILayout.Label("Set seed", EditorStyles.boldLabel);
+            EditorGUILayout.IntSlider(_seed, 0, 64000, new GUIContent("Seed"));
+            
+            GUILayout.Label("Set Scale (Recommended value between 0.001 & 1)", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(_perlinScaleFactor);
+            EditorGUILayout.Slider(_perlinHeightScale, 0.01f, 10, new GUIContent("Height Scale"));
+            
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            GUILayout.Label("fBM Settings", EditorStyles.boldLabel);
+            EditorGUILayout.IntSlider(_octaves, 1, 16, new GUIContent("Octaves"));
+            EditorGUILayout.Slider(_persistence, 0.01f, 5, new GUIContent("Persistence"));
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+            
+            GUILayout.Label("Replace heightmap", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(_replaceHeightMap);
+            if(GUILayout.Button("Generate Perlin Heights"))
+            {
+                terrain.PerlinHeightmap();
+            }
+        }
+        
+        // fBM Foldout UI
+        _showfBM = EditorGUILayout.Foldout(_showfBM, "fBM perlin noise");
+        if (_showfBM)
+        {
+            EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+            GUILayout.Label("fBM Perlin Noise");
+            _perlinSettingsTable = GUITableLayout.DrawTable(_perlinSettingsTable, serializedObject.FindProperty("perlinNoiseSettingsList"));
+            
+            GUILayout.Space(24);
+            EditorGUILayout.BeginHorizontal();
+                if(GUILayout.Button("ADD")) terrain.CreateNewSettings();
+                if(GUILayout.Button("REMOVE")) terrain.DeleteSettings();
+            EditorGUILayout.EndHorizontal();
+            
+            GUILayout.Label("Replace heightmap", EditorStyles.boldLabel);
+            EditorGUILayout.PropertyField(_replaceHeightMap);
+            if(GUILayout.Button("Apply")) terrain.PerlinSuperposition();
+        }
+
+
+        _showVoronoi = EditorGUILayout.Foldout(_showVoronoi, "Voronoi Tessellation");
+        if (_showVoronoi)
+        {
+            if(GUILayout.Button("Voronoi Tessellation")) terrain.VoronoiTessellation();
         }
         
         // Apply the changes made to the object
--- Assets/UPTT/Tool/UPTerrain.cs
+++ Assets/UPTT/Tool/UPTerrain.cs
@@ -3,33 +3,45 @@ using UnityEngine;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using UPTT.Tool;
+using UPTT.Tool.Utils;
 
 [ExecuteInEditMode]
 public class UPTerrain : MonoBehaviour
 {
-    
+
+    #region variables
     // Range for map generation based on Unity's Random
     [SerializeField] private Vector2 unityRandomRange = new Vector2(0, 0.1f);
     
     // Data for manipulating our terrain
     [SerializeField] private Terrain terrain;
     [SerializeField] private TerrainData terrainData;
-
+    [SerializeField] private bool replaceHeightmap = false;
+    
     // Height map parameters, the texture and scale factor
     [SerializeField] private Texture2D heightMapTexture;
     // Scale factor allows for stretching the heightmap to match our terrain
     [SerializeField] private Vector3 heightMapScale = Vector3.one;
     
-    
+    // Perlin noise parameters
+    [SerializeField] private int seed;
+    [SerializeField] private Vector2 perlinScaleFactor = new Vector2(0.01f,0.01f);
+    [SerializeField] private float perlinHeightScale = 0.07f;
+    [SerializeField] private int octaves = 4;
+    [SerializeField] private float persistence = 0.5f;
+    #endregion
+
+    [SerializeField] private List<PerlinNoiseSettings> perlinNoiseSettingsList
+        = new List<PerlinNoiseSettings>() {new PerlinNoiseSettings()};
+
     // Generate a height map using Unity's Random.Range function
     public void UnityRandomHeightMap()
     {
         Debug.Log("Generating Random Heights");
 
         // Create a new heightmap with dimension from our terrain data's heightmap resolution (will always be square i.e 1024x1024)
-        var heightMap = terrainData.GetHeights(0, 0,
-            terrainData.heightmapResolution,
-            terrainData.heightmapResolution);
+        var heightMap = Heightmap();
 
         // Iterate through all positions in the heightmap and give them a value between our unityRandomRange.x and .y
         for (var x = 0; x < terrainData.heightmapResolution; x++)
@@ -43,6 +55,114 @@ public class UPTerrain : MonoBehaviour
         // After generating the heightmap, apply it to the terrain. This will update instantly in the editor.
         terrainData.SetHeights(0,0, heightMap);
     }
+
+    public void PerlinHeightmap()
+    {
+        // Create a new heightmap with dimension from our terrain data's heightmap resolution (will always be square i.e 1024x1024)
+        var heightMap = terrainData.GetHeights(0, 0, terrainData.heightmapResolution, terrainData.heightmapResolution);
+
+        // Iterate through all positions in the heightmap and give them a value between our unityRandomRange.x and .y
+        for (var x = 0; x < terrainData.heightmapResolution; x++)
+        {
+            for (var y = 0; y < terrainData.heightmapResolution; y++)
+            {
+                if (replaceHeightmap)
+                {
+                    // set the height for the current x,y co-ordinate using fractal Brownian Motion
+                    heightMap[x, y] =
+                        UPTT.Tool.Utils.TerrainAlgorithms.FractalBrownianMotion(
+                            (x + seed)   * perlinScaleFactor.x, 
+                              (y + seed) * perlinScaleFactor.y,
+                              octaves,
+                              persistence,
+                              2.0f) * perlinHeightScale;
+                }
+                else
+                {
+                    /*
+                     *  Set the height for the current x,y co-ordinate usingfractal Brownian Motion
+                     *  As Perlin noise is essentially infinite, "seed" is used to change start point
+                     */
+                    heightMap[x, y] +=
+                        UPTT.Tool.Utils.TerrainAlgorithms.FractalBrownianMotion(
+                            (x + seed) * perlinScaleFactor.x,
+                            (y + seed) * perlinScaleFactor.y,
+                            octaves,
+                            persistence, 
+                            2.0f) * perlinHeightScale;
+                }
+            }
+        }
+        
+        // After generating the heightmap, apply it to the terrain. This will update instantly in the editor.
+        terrainData.SetHeights(0,0, heightMap);
+    }
+
+
+    public void VoronoiTessellation()
+    {
+        Debug.Log("VoronoiTessellation.");
+    }
+    
+    
+    #region PerlinSuperposition
+    // Combine all our given perlin noise
+    public void PerlinSuperposition()
+    {
+        var heightMap = Heightmap();
+
+        // Iterate through each pair of x,y co-ordinates
+        for (var x = 0; x < terrainData.heightmapResolution; x++)
+        {
+            for (var y = 0; y < terrainData.heightmapResolution; y++)
+            {
+                // Iterate through all of the perlin noise settings that have been given
+                foreach (var settings in perlinNoiseSettingsList)
+                {
+                    // add the value to the current x,y
+                    heightMap[x, y] += TerrainAlgorithms.FractalBrownianMotion(
+                        (x + settings.seed) * settings.scaleX,
+                        (y + settings.seed) * settings.scaleY,
+                        settings.fBMOctaves,
+                        settings.fBMPersistence) * settings.scaleHeight;
+                }
+            }
+        }
+        
+        terrainData.SetHeights(0, 0, heightMap);
+    }
+
+    public void CreateNewSettings()
+    {
+        perlinNoiseSettingsList.Add(new PerlinNoiseSettings());
+    }
+    public void DeleteSettings()
+    {
+        // Iterate through all the settings that have been marked for deletion and remove them
+        foreach (var settings in perlinNoiseSettingsList.Reverse<PerlinNoiseSettings>()
+            .Where(settings => settings.canDelete))
+        {
+            perlinNoiseSettingsList.Remove(settings);
+        }
+
+        // If no settings remain, add one back
+        if(perlinNoiseSettingsList.Count == 0) CreateNewSettings();
+    }
+    #endregion
+    
+    #region HeightmapUtils
+    private float[,] Heightmap()
+    {
+        if (!replaceHeightmap)
+        {
+            return terrainData.GetHeights(0, 0,
+                terrainData.heightmapResolution,
+                terrainData.heightmapResolution);
+        }
+        
+        return new float[terrainData.heightmapResolution, terrainData.heightmapResolution];
+    }
+    
     
     // Load a heightmap from our heightMapTexture
     public void LoadHeightMapFromTexture()
@@ -50,9 +170,9 @@ public class UPTerrain : MonoBehaviour
         var heightMap = new float[terrainData.heightmapResolution, terrainData.heightmapResolution];
 
         // Iterate 
-        for (int x = 0; x < terrainData.heightmapResolution; x++)
+        for (var x = 0; x < terrainData.heightmapResolution; x++)
         {
-            for (int y = 0; y < terrainData.heightmapResolution; y++)
+            for (var y = 0; y < terrainData.heightmapResolution; y++)
             {
                 // assign our heightmap x & y adjusted by our scale
                 var heightMapX = (int) (x * heightMapScale.x);
@@ -78,8 +198,11 @@ public class UPTerrain : MonoBehaviour
         var heightMap = new float[terrainData.heightmapResolution, terrainData.heightmapResolution];
         terrainData.SetHeights(0,0, heightMap);
     }
-    
-    
+
+    #endregion
+
+    #region Unity
+
     /**
      * Reset is called when the user hits the Reset button in the Inspector's context menu or when adding the component the first time.
      * This function is only called in editor mode. Reset is most commonly used to give good default values in the Inspector.
@@ -108,7 +231,6 @@ public class UPTerrain : MonoBehaviour
 
         this.gameObject.tag = "UPTerrain";
     }
-
     private static void AddTag(SerializedProperty tagsProperty, string tag)
     {
         bool isFound = false;
@@ -137,6 +259,7 @@ public class UPTerrain : MonoBehaviour
         SerializedProperty newTag = tagsProperty.GetArrayElementAtIndex(0);
         newTag.stringValue = tag;
     }
+    #endregion
 
     
 }
